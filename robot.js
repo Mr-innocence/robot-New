ad0421de905e34b21ef7b83e4ea15be34a1a9cd1
Xiyue Wang <xwan0077@student.monash.edu>
2019-06-19 20:51:41 +1000

    complete move

--- robot.js
+++ robot.js
@@ -4,80 +4,104 @@ function Position(x, y) {
 }
 
 Position.prototype = {
-    toIndex: function(mapWidth) {
+    toIndex: function (mapWidth) {
         return this.x + this.y * mapWidth;
     }
 }
 
-function MovingObject(position, icon) {
+function Robot(position, icon)
+{
     this.position = position;
     this.icon = icon;
 }
 
-function Game(mapWidth, mapHeight, robot) {
+function Game(mapWidth, mapHeight, robot)
+{
     this.mapWidth = mapWidth;
     this.mapHeight = mapHeight;
     this.robot = robot;
-    this.history = [];
 
-    this.init = function() {
+    this.init = function()
+    {
         this.robot.position.y = 3;
-    };
-    
-    this.onCommandUp = function() {
-        this.history.push(this.robot.position);
-        var targetPosition = new Position(this.robot.position.x, this.robot.position.y - 1);
-        this.move(targetPosition);
+        this.robot.position.x = 1;
     };
 
-    this.onCommandBack = function() {
-        this.move(this.history.pop());
-    };
-    
-    this.availablePosition = function(newPosition) {
-        if (newPosition.x >= 0 && newPosition.x < this.mapWidth
-            && newPosition.y >= 0 && newPosition.y < this.mapHeight
-        ) {
-            return true;
-        } else {
-            return false;
-        }
+    this.onCommandUp = function()
+    {
+        var targetPostion = new Position(this.robot.position.x, this.robot.position.y-1);
+        this.move(targetPostion);
+        
     };
 
-    this.move = function(newPosition) {
-        if (this.availablePosition(newPosition)) {
-            this.robot.position = newPosition;
-            // this.state = Object.assign({}, this.state, {robotPosition: newPosition});
-            this.render();
-            return true;
-        } else {
-            return false;
-        }
+    this.onCommandRight = function()
+    {
+        var targetPostion = new Position(this.robot.position.x + 1, this.robot.position.y);
+        this.move(targetPostion);
+        
     };
+
+    this.onCommandDown = function()
+    {
+        var targetPostion = new Position(this.robot.position.x, this.robot.position.y + 1);
+        this.move(targetPostion);
+        
+    };
+
+    this.onCommandLeft = function()
+    {
+        var targetPostion = new Position(this.robot.position.x - 1, this.robot.position.y);
+        this.move(targetPostion);
+        
+    };
+
+    this.onCommandReset = function()
+    {
+        var targetPostion = new Position(0,0);
+        this.move(targetPostion);
+    }
 
     this.render = function() {
         var mapCells = document.querySelectorAll('.grid-cell');
         var robotIndex = this.robot.position.toIndex(this.mapWidth);
-        mapCells.forEach((aCell, i) => {
+        for (var i = 0; i < mapCells.length; i++) {
             if (i === robotIndex) {
-                mapCells[i].innerHTML = this.robot.icon;
+                mapCells[i].innerHTML = 'R';
             } else {
                 mapCells[i].innerHTML = '';
             }
-        })
-        // for (var i = 0; i < mapCells.length; i++) {
-        //     if (i === robotIndex) {
-        //         mapCells[i].innerHTML = 'R';
-        //     } else {
-        //         mapCells[i].innerHTML = '';
-        //     }
-        // }
+        }
+    };
+
+    this.avaiablePosition = function(newPosition)
+    {
+        if(newPosition.x >= 0 && newPosition.x < mapWidth
+            && newPosition.y >= 0 && newPosition.y < mapHeight)
+        {
+            return true;
+        }           
+        else
+        {
+            return false;
+        }
+    };
+
+    this.move = function(newPostion)
+    {
+        if(this.avaiablePosition(newPostion))
+        {
+            this.robot.position = newPostion;
+            this.render();
+        }
     }
 }
 
-var initPosition = new Position(0, 0);
-var robot = new MovingObject(initPosition, "R");
-var game = new Game(4, 4, robot)
+
+
+
+var initP = new Position(0,0);
+var robot = new Robot(initP,"R");
+var game = new Game(4,4,robot);
 
 game.init();
 game.render();
